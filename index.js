be083d37a36cc940f9198d83d2602b05fc3159c1
craig-mulligan <craig@resin.io>
2017-06-15 08:54:45 +0100

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+var axios = require('axios');
+
+var instance = axios.create({
+  baseURL: 'https://wunameaas.herokuapp.com',
+  headers: {'Accept': 'application/json'}
+});
+
+function wuami(name) {
+  return instance.get('/wuami/' + name).then(function (response) {
+    return response.data.message;
+  });
+}
+
+function enterthewu(name) {
+  return instance.get('/enterthewu/' + name).then(function (response) {
+    return response.data.message;
+  });
+}
+
+module.exports = {
+  wuami: wuami,
+  enterthewu: enterthewu
+}
